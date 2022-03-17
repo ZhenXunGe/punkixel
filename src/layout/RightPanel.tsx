170df86ef9ddb4484af1b66ddbcc177d7f31645e
Allenscript <990479310@qq.com>
2022-03-17 12:41:27 +0800

    feat: add alieninfo area and update some image

--- src/layout/RightPanel.tsx
+++ src/layout/RightPanel.tsx
@@ -23,13 +23,26 @@ function AlienInfo() {
       <div className="alien-basic-avator">
         <img src={alien}></img>
       </div>
+      <div className="alien-basic-info">
+          <p>Gru02</p> 
+          <div className='skill'>
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
+          </div>
+        </div>
       <div className="right">
-        <div> Speed: 200</div>
-        <div> Attracted by: night view</div>
+        <div className='speed'>200</div>
+        <div className='strack'>Night view</div>
       </div>
     </div>
+    <div className='summary'>
     <div> Gru02 from planet 0x3245 is about to arrive Z-city within arount 20 minutes </div>
-    <div> Gru02 carries plenty of native rocks from planet 0x3245 which is the receipt of dye [#dye2] [#dye3]</div>
+    {/* <div> Gru02 carries plenty of native rocks from planet 0x3245 which is the receipt of dye [#dye2] [#dye3]</div> */}
+    <button className='more'><p>MORE</p></button>
+    </div>
   </div>);
 }
 
@@ -37,20 +50,20 @@ export function RightPanel() {
   const panel = useAppSelector(selectPanel);
   return (
     <>
-    { panel == "home" && <HomePanel key="home-panel" ></HomePanel>}
-    { panel == "world" && <WorldPanel></WorldPanel>}
-    { panel == "vote" && <VotePanel></VotePanel>}
-    { panel == "market" && <MarketPanel></MarketPanel>}
-    <Events></Events>
-    <div className="alien-info">
-      <AlienInfo></AlienInfo>
-    </div>
-    <div className='bottom_left'>
-      <img src={btm_left}></img>
-    </div>
-    <div className='bottom_right'>
-      <img src={btm_right}></img>
-    </div>
+      {panel == "home" && <HomePanel key="home-panel" ></HomePanel>}
+      {panel == "world" && <WorldPanel></WorldPanel>}
+      {panel == "vote" && <VotePanel></VotePanel>}
+      {panel == "market" && <MarketPanel></MarketPanel>}
+      <Events></Events>
+      <div className="alien-info">
+        <AlienInfo></AlienInfo>
+      </div>
+      <div className='bottom_left'>
+        <img src={btm_left}></img>
+      </div>
+      <div className='bottom_right'>
+        <img src={btm_right}></img>
+      </div>
     </>
   );
 }
