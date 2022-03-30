a2b7d34d0e2969c220a3176bea794c774644fcaa
Allenscript <990479310@qq.com>
2022-03-30 19:14:48 +0800

    feat: add dyeDrop modal

--- src/modals/palette.tsx
+++ src/modals/palette.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Container, Modal } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { fromDrop, getPalette } from "../data/palette";
 
@@ -8,7 +8,7 @@ interface PaletteProps {
   paletteId: string;
 }
 
-export default function PaletteInfo(prop:PaletteProps) {
+export default function PaletteInfo(prop: PaletteProps) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -19,9 +19,9 @@ export default function PaletteInfo(prop:PaletteProps) {
 
     <>
       <div className="button" onClick={() => handleShow()}>
-            {prop.paletteId}
+        {prop.paletteId}
       </div>
-      <Modal show={show} onHide={handleClose} centered>
+      {/* <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
           <Modal.Title>Palette {palette.name}</Modal.Title>
         </Modal.Header>
@@ -35,6 +35,41 @@ export default function PaletteInfo(prop:PaletteProps) {
             Ok
           </Button>
         </Modal.Footer>
+      </Modal> */}
+      <Modal show={show} aria-labelledby="contained-modal-title-vcenter" centered dialogClassName="modal-90w">
+
+        <Modal.Body className="show-grid">
+          <Container>
+            <button className="closeBtn" onClick={handleClose}></button>
+            <div className="dyeDrop_area">
+              <div className="dye_area">
+                <div className="palette_info">
+                  <div className="dyeTop">
+                  <div className="paletteName">
+                  {palette.name} palette
+                  </div>
+                  <div className="categoryName">
+                    Basic
+                  </div>
+                  </div>
+                  <ul className="palettePreview">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                  <div className="pph">{palette.pph}</div>
+                </div>
+              </div>
+            </div>
+
+          </Container>
+        </Modal.Body>
+
       </Modal>
     </>
   );
